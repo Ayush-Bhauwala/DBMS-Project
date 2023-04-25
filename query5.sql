7a08f1282ac08416abf7a24bad26f350188ea765
Arnav Arvind <f20212709@hyderabad.bits-pilani.ac.in>
2023-04-26 01:08:13 +0530

    inserted cursor into query 5 and 6

--- query5.sql
+++ query5.sql
@@ -1,15 +1,41 @@
 CREATE OR REPLACE PROCEDURE SearchPropertyForRent(City IN PropertyTable.City%TYPE)
 AS
+    CURSOR property_cursor IS
+        SELECT * 
+        FROM (PropertyTable LEFT OUTER JOIN HistoryTable ON PropertyTable.PropertyID = HistoryTable.PropertyID)
+        WHERE PropertyTable.City = City
+        AND
+        (
+        HistoryTable.Start_date = NULL
+        OR
+        trunc(sysdate) BETWEEN HistoryTable.Start_date AND HistoryTable.End_date
+        );
+    v_property property_cursor%ROWTYPE;
+
 BEGIN
 
-SELECT * FROM (PropertyTable LEFT OUTER JOIN HistoryTable ON PropertyTable.PropertyID = HistoryTable.PropertyID)
-WHERE
-PropertyTable.City = City
-AND
-(
-HistoryTable.Start_date = NULL
-OR
-trunc(sysdate) BETWEEN HistoryTable.Start_date AND HistoryTable.End_date
-);
+    OPEN property_cursor;
+    FETCH property_cursor INTO v_property;
+    IF (property_cursor%FOUND) THEN 
+        -- Print property details
+        DBMS_OUTPUT.PUT_LINE('Property ID: ' || v_property.PropertyID);
+        DBMS_OUTPUT.PUT_LINE('Owner ID: ' || v_property.OwnerID);
+        DBMS_OUTPUT.PUT_LINE('Date Available From: ' || v_property.Date_available_from);
+        DBMS_OUTPUT.PUT_LINE('Date Available Till: ' || v_property.Date_available_till);
+        DBMS_OUTPUT.PUT_LINE('Rent per Month: ' || v_property.Rent_per_month);
+        DBMS_OUTPUT.PUT_LINE('Percent Annual Hike: ' || v_property.Percent_annual_hike);
+        DBMS_OUTPUT.PUT_LINE('Total Area: ' || v_property.Total_area);
+        DBMS_OUTPUT.PUT_LINE('Plinth Area: ' || v_property.Plinth_area);
+        DBMS_OUTPUT.PUT_LINE('Floor No.: ' || v_property.Floor_no);
+        DBMS_OUTPUT.PUT_LINE('Construction Year: ' || v_property.Construction_year);
+        DBMS_OUTPUT.PUT_LINE('Locality: ' || v_property.Locality);
+        DBMS_OUTPUT.PUT_LINE('City: ' || v_property.City);
+        DBMS_OUTPUT.PUT_LINE('State: ' || v_property.State);
+        DBMS_OUTPUT.PUT_LINE('Pincode: ' || v_property.Pincode);
+    ELSE
+        -- Print message if no properties are found
+        DBMS_OUTPUT.PUT_LINE('There is no property in this city.');
+    END IF;
+    CLOSE property_cursor;
 
 END;
--- query6.sql
+++ query6.sql
@@ -1,9 +1,27 @@
 CREATE OR REPLACE PROCEDURE GetRentHistory(PropertyID IN HistoryTable.PropertyID%TYPE)
 AS
+    CURSOR history_cursor IS    
+        SELECT *
+        FROM HistoryTable
+        WHERE HistoryTable.PropertyID = PropertyID;
+    v_history history_cursor%ROWTYPE;
+
 BEGIN
 
-SELECT * FROM HistoryTable
-WHERE
-HistoryTable.PropertyID = PropertyID;
-
+    OPEN history_cursor;
+    FETCH history_cursor INTO v_history;
+    IF (history_cursor%FOUND) THEN 
+        -- Print property details
+        DBMS_OUTPUT.PUT_LINE('Property ID: ' || v_history.PropertyID);
+        DBMS_OUTPUT.PUT_LINE('Start Date: ' || v_history.Start_date);
+        DBMS_OUTPUT.PUT_LINE('End Date: ' || v_history.End_date);
+        DBMS_OUTPUT.PUT_LINE('Tenant ID: ' || v_history.TenantID);
+        DBMS_OUTPUT.PUT_LINE('Agency Commission: ' || v_history.Agency_commission);
+        DBMS_OUTPUT.PUT_LINE('Rent: ' || v_history.Rent);
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('No rent history found for the given property');
+    END IF;
+    CLOSE history_cursor;
+    
 END;
+/
