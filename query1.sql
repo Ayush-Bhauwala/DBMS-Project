0ce4750a6ae4bd523194b5958a8e97fb7cef8b9d
siddharthyayavaram <68839837+siddharthyayavaram@users.noreply.github.com>
2023-04-26 17:54:12 +0530

    1,2 done

--- query1.sql
+++ query1.sql
@@ -1,6 +1,8 @@
 CREATE OR REPLACE PROCEDURE InsertPropertyRecord(
+    bedroom IN ResidentialTable.Bedroom_no%TYPE,
+    fac IN FacilitiesTable.Facility%TYPE,
+    userpass IN UserTable.Login_password%TYPE,
     aadhaarID_input IN UserTable.AadhaarID%TYPE,
-    PropertyID IN PropertyTable.PropertyID%TYPE,
     Date_available_from IN PropertyTable.Date_available_from%TYPE,
     Date_available_till IN PropertyTable.Date_available_till%TYPE,
     Rent_per_month IN PropertyTable.Rent_per_month%TYPE,
@@ -15,28 +17,68 @@ CREATE OR REPLACE PROCEDURE InsertPropertyRecord(
     Pincode IN PropertyTable.Pincode%TYPE
 ) AS
   v_user_exists NUMBER;
+  ismanager NUMBER;
+  passvariable UserTable.Login_password%TYPE;
+  --v_max_id PropertyTable.PropertyID%TYPE;
+  Property_ID PropertyTable.PropertyID%TYPE;
 BEGIN
-  -- Check if the given Aadhaar ID exists in UserTable
-SELECT COUNT(*) INTO v_user_exists FROM UserTable WHERE AadhaarID = aadhaarID_input;
+  
+  -- Retrieve the password for the given Aadhaar ID from UserTable
+  SELECT Login_password INTO passvariable from UserTable where AadhaarID=aadhaarID_input;
 
-  IF v_user_exists > 0 THEN
-    -- If user exists, insert a new record into PropertyTable with the given property details
-    INSERT INTO PropertyTable(
-      PropertyID, OwnerID, Date_available_from, Date_available_till,
-      Rent_per_month, Percent_annual_hike, Total_area, Plinth_area,
-      Floor_no, Construction_year, Locality, City, State, Pincode
-    )
-    VALUES(
-      PropertyID, aadhaarID_input, Date_available_from, Date_available_till,
-      Rent_per_month, Percent_annual_hike, Total_area, Plinth_area,
-      Floor_no, Construction_year, Locality, City, State, Pincode
-    );
+  SELECT COUNT(*) INTO ismanager FROM ManagerTable WHERE AadhaarID = aadhaarID_input;
 
-    DBMS_OUTPUT.PUT_LINE('New property record inserted successfully.');
-  ELSE
-    -- If user does not exist, display error message
-    DBMS_OUTPUT.PUT_LINE('Error: Aadhaar ID ' || aadhaarID_input || ' does not exist in UserTable.');
-  END IF;
+  IF passvariable=userpass THEN
+    --DBMS_OUTPUT.PUT_LINE('IN IF');
+
+    --Check if the user with the given Aadhaar ID is not a manager
+    IF ismanager=0 THEN
+
+      --DBMS_OUTPUT.PUT_LINE('IN IF');
+
+      --Check if the given Aadhaar ID exists in UserTable
+      SELECT COUNT(*) INTO v_user_exists FROM UserTable WHERE AadhaarID = aadhaarID_input;
+
+      IF v_user_exists > 0 THEN
+        -- Retrieve the maximum property ID from PropertyTable and increment it by 1 to get the new property ID
+        SELECT NVL(MAX(PropertyID), 0) + 1 INTO Property_ID FROM PropertyTable;
+        -- Insert the new record into PropertyTable with the given property details
+        INSERT INTO PropertyTable(
+          PropertyID, OwnerID, Date_available_from, Date_available_till,
+          Rent_per_month, Percent_annual_hike, Total_area, Plinth_area,
+          Floor_no, Construction_year, Locality, City, State, Pincode
+        )
+        VALUES(
+          Property_ID, aadhaarID_input, Date_available_from, Date_available_till,
+          Rent_per_month, Percent_annual_hike, Total_area, Plinth_area,
+          Floor_no, Construction_year, Locality, City, State, Pincode
+        );
+        -- Insert the owner record into OwnerTable with the newly generated property ID
+        INSERT INTO OwnerTable (AadhaarID) VALUES (aadhaarID_input);
+        -- Insert the facilities record into FacilitiesTable with the newly generated property ID
+        INSERT INTO FacilitiesTable (PropertyID, Facility) VALUES (Property_ID, fac);
+        -- Insert the residential or commercial record depending on the number of bedrooms
+        IF bedroom > 0 THEN
+          INSERT INTO ResidentialTable (PropertyID, Bedroom_no) VALUES (Property_ID, bedroom);
+        ELSE
+          INSERT INTO CommercialTable (PropertyID) VALUES (Property_ID);
+        END IF;
+        DBMS_OUTPUT.PUT_LINE('New property record inserted successfully.');
+      ELSE
+        -- If user does not exist, display error message
+        DBMS_OUTPUT.PUT_LINE('Error: Aadhaar ID ' || aadhaarID_input || ' does not exist in UserTable.');
+          END IF;
+
+      ELSE
+          DBMS_OUTPUT.PUT_LINE('Managers cannot add property');
+
+      END IF;
+
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Incorrect password');
+
+
+    END IF;
 END;
 /
 
--- query2.sql
+++ query2.sql
@@ -1,36 +1,64 @@
-CREATE OR REPLACE PROCEDURE GetPropertyRecords(ownID IN CHAR)
+CREATE OR REPLACE PROCEDURE GetPropertyRecords(
+    ownID IN OwnerTable.AadhaarID%TYPE,
+    qID IN OwnerTable.AadhaarID%TYPE,
+    userpass IN UserTable.Login_password%TYPE
+)
 AS
     CURSOR property_cursor IS 
         SELECT *
         FROM PropertyTable
         WHERE OwnerID = ownID;
     v_property property_cursor%ROWTYPE;
+    passvariable UserTable.Login_password%TYPE;
+    ismanager NUMBER;
+
 BEGIN
-    OPEN property_cursor;
-    FETCH property_cursor INTO v_property;
-    IF (property_cursor%FOUND) THEN
-        -- Print property details
-        DBMS_OUTPUT.PUT_LINE('Property ID: ' || v_property.PropertyID);
-        DBMS_OUTPUT.PUT_LINE('Owner ID: ' || v_property.OwnerID);
-        DBMS_OUTPUT.PUT_LINE('Date Available From: ' || v_property.Date_available_from);
-        DBMS_OUTPUT.PUT_LINE('Date Available Till: ' || v_property.Date_available_till);
-        DBMS_OUTPUT.PUT_LINE('Rent per Month: ' || v_property.Rent_per_month);
-        DBMS_OUTPUT.PUT_LINE('Percent Annual Hike: ' || v_property.Percent_annual_hike);
-        DBMS_OUTPUT.PUT_LINE('Total Area: ' || v_property.Total_area);
-        DBMS_OUTPUT.PUT_LINE('Plinth Area: ' || v_property.Plinth_area);
-        DBMS_OUTPUT.PUT_LINE('Floor No.: ' || v_property.Floor_no);
-        DBMS_OUTPUT.PUT_LINE('Construction Year: ' || v_property.Construction_year);
-        DBMS_OUTPUT.PUT_LINE('Locality: ' || v_property.Locality);
-        DBMS_OUTPUT.PUT_LINE('City: ' || v_property.City);
-        DBMS_OUTPUT.PUT_LINE('State: ' || v_property.State);
-        DBMS_OUTPUT.PUT_LINE('Pincode: ' || v_property.Pincode);
+
+    SELECT Login_password INTO passvariable from UserTable where AadhaarID=qID;
+
+    SELECT COUNT(*) INTO ismanager FROM ManagerTable WHERE AadhaarID = qID;
+
+    IF passvariable=userpass THEN
+
+        IF ((ismanager>0) or (OwnID=qID)) THEN
+            OPEN property_cursor;
+            FETCH property_cursor INTO v_property;
+            IF (property_cursor%FOUND) THEN
+                -- Print table header
+                DBMS_OUTPUT.PUT_LINE('+---------------+---------------+----------------------+----------------------+-----------------+----------------------+----------------+----------------+-----------+------------------+------------+------------+------------+');
+                DBMS_OUTPUT.PUT_LINE('| Property ID   | Owner ID      | Date Available From  | Date Available Till  | Rent per Month  | Percent Annual Hike  | Total Area     | Plinth Area    | Floor No. | Construction Year | Locality   | City       | State      |');
+                DBMS_OUTPUT.PUT_LINE('+---------------+---------------+----------------------+----------------------+-----------------+----------------------+----------------+----------------+-----------+------------------+------------+------------+------------+');
+                
+                -- Print property details
+                LOOP
+                    DBMS_OUTPUT.PUT_LINE('| ' || LPAD(v_property.PropertyID, 13) || ' | ' || LPAD(v_property.OwnerID, 13) || ' | ' || LPAD(v_property.Date_available_from, 20) || ' | ' || LPAD(v_property.Date_available_till, 20) || ' | ' || LPAD(v_property.Rent_per_month, 15) || ' | ' || LPAD(v_property.Percent_annual_hike, 22) || ' | ' || LPAD(v_property.Total_area, 14) || ' | ' || LPAD(v_property.Plinth_area, 14) || ' | ' || LPAD(v_property.Floor_no, 9) || ' | ' || LPAD(v_property.Construction_year, 18) || ' | ' || LPAD(v_property.Locality, 11) || ' | ' || LPAD(v_property.City, 11) || ' | ' || LPAD(v_property.State, 11) || ' | ');
+                    FETCH property_cursor INTO v_property;
+                    EXIT WHEN property_cursor%NOTFOUND;
+                END LOOP;
+
+                -- Print table footer
+                DBMS_OUTPUT.PUT_LINE('+---------------+---------------+----------------------+----------------------+-----------------+----------------------+----------------+----------------+-----------+------------------+------------+------------+------------+');
+
+            ELSE
+                -- Print message if no properties are found
+                DBMS_OUTPUT.PUT_LINE('This person doesn''t own any property.');
+            END IF;
+            CLOSE property_cursor;
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('You cannot view the property details with current permissions.');
+
+        END IF;
+
+
     ELSE
-        -- Print message if no properties are found
-        DBMS_OUTPUT.PUT_LINE('This person doesn''t own any property.');
+        DBMS_OUTPUT.PUT_LINE('Incorrect password.');
+
     END IF;
-    CLOSE property_cursor;
+
+
 END;
 /
 
 
-EXEC GetPropertyRecords('123456789012');
+
+EXEC GetPropertyRecords('123456789012','123456789012','password123');
